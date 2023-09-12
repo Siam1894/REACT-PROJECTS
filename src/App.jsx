28579856c37b43b2e2b759e29ba4d300e5017185
Siam1894 <mojidmdabdul1894@gmail.com>
2023-09-12 17:51:49 +0600

    added the few files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/header/Header'
+import Blogs from './Components/Blogs/Blogs'
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
